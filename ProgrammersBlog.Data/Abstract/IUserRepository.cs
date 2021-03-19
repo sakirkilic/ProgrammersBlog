03a1c30bf0248a57ec8d55921ff3cc2f82ef92fe
sakirkilic <skrklc@windowslive.com>
2021-03-19 11:27:45 +0300

    Data Katmanı Oluşturuldu

---
+++ ProgrammersBlog.Data/Abstract/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+	public interface IUserRepository : IEntityRepository<User>
+	{
+		//Task<User> GetEmailAsync(Expression<Func<User, bool>> predicate, params Expression<Func<User, object>>[] includeProperties);
+	}
+}
